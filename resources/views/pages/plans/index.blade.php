acee7321754b9f02a10a0851c06d874d48552aa0
dudox <trybemark@gmail.com>
2020-08-11 22:33:42 +0100

    updates

--- resources/views/pages/plans/index.blade.php
+++ resources/views/pages/plans/index.blade.php
@@ -14,15 +14,38 @@
                 <p class="text-muted">Flexible plans allows you to easily run your business ads with sellfast.ng</p>
             </div>
         </div>
-
         <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 bg-back" data-link="{{ route('subscription') }}" >
+                    <div class="card-body card2" style=" border-radius: 20px !important">
+                        <div class="row text-white">
+                            <div class="col-md-12">
+                                <h6 class="mb-0">PREMIUM PLAN</h6>
+                                <p class="mt-0 font-weight-bolder text-uppercase">Get two adverts per month</p>
+                            </div>
+                        </div>
+                        <div class="d-flex mt-4 text-white align-items-end">
+                            <div class="amount">
+                                <h6 class="mb-0">TOTAL AMOUNT</h6>
+                                <p class="mt-0 font-weight-bolder mb-0" style="font-size: 20px">NGN 500.00</p>
+                            </div>
+                            <div class="ml-auto justify-content-end">
+                                <a  class="btn btn-dark rounded-pill transparent" style="font-size: 10px">Terms & Condition <i class="fa fa-long-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row my-3">
             <div class="col-md-12">
                 <div class="card border-0 bg-back" data-link="{{ route('basic') }}" >
                     <div class="card-body card1" style=" border-radius: 20px !important">
                         <div class="row text-white">
                             <div class="col-md-12">
-                                <h6 class="mb-0">1. BASIC PLAN</h6>
-                                <p class="mt-0 font-weight-bolder text-uppercase">Get one adverts per month</p>
+                                <h6 class="mb-0">BASIC PLAN</h6>
+                                <p class="mt-0 font-weight-bolder text-uppercase">Get one advert per month</p>
                             </div>
                         </div>
                         <div class="d-flex mt-4 text-white align-items-end">
@@ -39,29 +62,7 @@
             </div>
         </div>
 
-        <div class="row my-3">
-            <div class="col-md-12">
-                <div class="card border-0 bg-back" data-link="{{ route('subscription') }}" >
-                    <div class="card-body card2" style=" border-radius: 20px !important">
-                        <div class="row text-white">
-                            <div class="col-md-12">
-                                <h6 class="mb-0">2. PREMIUM PLAN</h6>
-                                <p class="mt-0 font-weight-bolder text-uppercase">Get two advert per month</p>
-                            </div>
-                        </div>
-                        <div class="d-flex mt-4 text-white align-items-end">
-                            <div class="amount">
-                                <h6 class="mb-0">TOTAL AMOUNT</h6>
-                                <p class="mt-0 font-weight-bolder mb-0" style="font-size: 20px">NGN 500.00</p>
-                            </div>
-                            <div class="ml-auto justify-content-end">
-                                <a  class="btn btn-dark rounded-pill transparent" style="font-size: 10px">Terms & Condition <i class="fa fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
         <div style="position: absolute; width:100%; bottom: 5px" class="row mt-2">
             <div class="col-md-12 text-center">
                 <p class="font-weight-bolder">Powered by WorkPride</p>
