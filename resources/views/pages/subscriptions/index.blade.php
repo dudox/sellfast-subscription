74d4ee5aea7ec086757c3d9d2a2f846d604605ec
dudox <trybemark@gmail.com>
2020-08-11 22:40:26 +0100

    updates

--- resources/views/pages/subscriptions/index.blade.php
+++ resources/views/pages/subscriptions/index.blade.php
@@ -1,107 +1,212 @@
+
 @extends('layouts.app')
 
-
 @section('contents')
-<div class="row justify-content-center">
-    <div class="col-md-5">
-        <div class="row  my-3 ml-1">
-            <img src="{{asset('img/logo.png')}}" style="width: 70px" alt="">
-        </div>
-        {{-- <div class="row">
-            <div class="col">
-                <span class="numberCircle border-success text-success">1</span> <span class="font-weight-bold" style="font-size: 14px">&nbsp;&nbsp;&nbsp;Choose plan</span>
+
+    <div class="row justify-content-center">
+        <div class="col-md-3">
+            <div class="row justify-content-center mt-2">
+                <img src="{{asset('img/logo.png')}}" style="width: 50px" alt="">
             </div>
-            <div class="col">
-                <span class="numberCircle text-muted">2</span> <span class="font-weight-bold text-muted" style="font-size: 14px">&nbsp;&nbsp;&nbsp;Payment</span>
-            </div>
-
-        </div> --}}
-
-        <div class="row mt-3">
-            <div class="col-md-12">
-                <p class="font-weight-bolder mb-0" style="font-size: 19px">Choose a plan</p>
-                <p class="text-muted">Flexible plans allows you to easily run your business ads with sellfast.ng</p>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card" style="background: #f8fafc; border-radius: 6px">
-                    <div class="card-body ">
-                        <div class="row">
-                            <div class="col-3">
-                                <i class="fa fa-empire fa-3x tempColor"></i>
-
-                            </div>
-                            <div class="col">
-                                <p class="font-weight-bolder p-0 m-0" style="font-size: 14px">Basic Plan</p>
-                                <p class="p-0 m-0"><sup style="font-size: 20px" class="text-muted">&#8358;</sup> <span class="tempColor font-weight-bold" style="font-size: 26px">500.00</span> <sub class="text-muted">/ month</sub></p>
-                            </div>
-                        </div>
-                        <hr>
-                        <ul>
-                            <li><i class="fa fa-check tempColor font-weight-bolder"></i> &nbsp;&nbsp;Promote your  <span class="font-weight-bold">business brand</span></li>
-                            <li><i class="fa fa-check tempColor font-weight-bolder"></i> &nbsp;&nbsp;1 business banner <span class="font-weight-bold">promotion</span></li>
-                            <li><i class="fa fa-check tempColor font-weight-bolder"></i> &nbsp;&nbsp;1 time payment <span class="font-weight-bold">charges</span></li>
-                            <li><i class="fa fa-times text-danger font-weight-bolder"></i> &nbsp;&nbsp;<span class="font-weight-bold">Notification</span> supports reminder</li>
-
-                        </ul>
-                        <div class="row mt-4">
-                            <div class="col-md-12">
-                                <a href="{{ route('basic') }}" class="btn btn-success btn-block py-3 font-weight-bolder">Choose Basic Plan <i class="fa fa-long-arrow-right ml-3"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row my-3">
-            <div class="col-md-12">
-                <div class="card" style="background: #f8fafc; border-radius: 6px">
-                    <div class="card-body ">
-                        <div class="row">
-                            <div class="col-3">
-                                <i class="fa fa-connectdevelop fa-3x tempColor"></i>
-
-                            </div>
-                            <div class="col">
-                                <p class="font-weight-bolder p-0 m-0" style="font-size: 14px">Subscription Plan</p>
-                                <p class="p-0 m-0"><sup style="font-size: 20px" class="text-muted">&#8358;</sup> <span class="tempColor font-weight-bold" style="font-size: 26px">500</span> <sub class="text-muted">/ month</sub></p>
-                            </div>
-                            <div class="ml-auto">
-                                <span class="badge badge-dark badge-pill" style="font-size: 8px">POPULAR <i class="fa fa-check-circle-o tempColor"></i></span>
-                            </div>
-                        </div>
-                        <hr>
-                        <ul>
-                            <li><i class="fa fa-check tempColor font-weight-bolder"></i> &nbsp;&nbsp;Promote your  <span class="font-weight-bold">business brand</span></li>
-                            <li><i class="fa fa-check tempColor font-weight-bolder"></i> &nbsp;&nbsp;2 business banner <span class="font-weight-bold">promotion</span></li>
-                            <li><i class="fa fa-check tempColor font-weight-bolder"></i> &nbsp;&nbsp;1 time payment <span class="font-weight-bold">charges</span></li>
-                            <li><i class="fa fa-check tempColor font-weight-bolder"></i> &nbsp;&nbsp;<span class="font-weight-bold">Notification</span> supports reminder</li>
-                        </ul>
-                        <div class="row mt-4">
-                            <div class="col-md-12">
-                                <a href="" class="btn btn-success btn-block py-3 font-weight-bolder">Choose Basic Plan <i class="fa fa-long-arrow-right ml-3"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
             <div class="col-md-12 text-center">
-                <p class="font-weight-bolder">Powered by WorkPride</p>
+                <h4 class="text-uppercase font-weight-bolder mt-3">
+                    subcription payment
+                </h4>
             </div>
+
+
+            <h5 class="text-uppercase">1. Select your payment method</h5>
+            <div class="d-flex">
+                <a class="col shadow-sm py-2 text-center option " data-type="credit card"  rel="#card">
+                    <div class="icon text-center">
+                        <i class="fa fa-credit-card fa-3x font-weight-light" aria-hidden="true"></i>
+                    </div>
+                    <div class=" text-center ">
+                        <p class="param">CREDIT CARD</p>
+                    </div>
+                </a>
+                <a class="col shadow-sm py-2 text-center option" data-type="bank transfer" rel="#bank" >
+                    <div class="icon text-center">
+                        <i class="fa fa-bank fa-3x" aria-hidden="true"></i>
+                    </div>
+                    <div class="param text-center ">BANK TRANSFER</div>
+                </a>
+                {{-- <a class="col shadow py-2 text-center option">
+                    <div class="icon text-center">
+                        <i class="fa fa-hashtag fa-lg" aria-hidden="true"></i>
+                    </div>
+                    <div class="param text-center ">USSD</div>
+                </a> --}}
+            </div>
+            <hr class="shadow-lg">
+            <div class="d-flex shadow-sm selected-results my-2">
+
+                <label class="my-2 text-uppercase" style="font-size: 13px"><i class="fa fa-check-circle-o text-success" aria-hidden="true"></i> <span class="font-weight-bolder">Method:</span> <span class="type"></span></label>
+            </div>
+            <header class="credit tab_contents" id="card" style="display: none;">
+                <div class="row">
+                    <div class="col-md-12">
+                        <img src="{{asset('img/sample3.png')}}" style="width: 400px; margin-top: -20px" alt="">
+                        <img src="{{ asset('img/badge.png') }}" style="margin: -90px 0 0 0;" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-12">
+                        <h5>2. PLEASE ENTER YOUR INFORMATION</h5>
+                        <form method="POST" id="bank" action="{{ route('flutterwave') }}" data-type="card">
+                            @csrf
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for=""><i class="fa fa-user-o"></i> FULL NAME</label>
+                                    <input type="text" name="fullname" class="form-control bg-gray" placeholder="" value="" required>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label for=""><i class="fa fa-envelope"></i> EMAIL ADDRESS</label>
+                                    <input type="text" name="email_address" class="form-control bg-gray" required>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label for=""><i class="fa fa-phone"></i> PHONE NUMBER</label>
+                                    <input type="text" name="phone" class="form-control bg-gray" required>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label for=""><i class="fa fa-instagram"></i> INSTAGRAM NAME / HANDLE</label>
+                                    <input type="text" name="username" class="form-control bg-gray" required>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-bottom">
+                                <div class="">
+                                    <div class="badge badge-white text-uppercase">Total Amount</div><br/>
+                                    <label class="" style="font-size: 30px">$ 500.00</label>
+                                </div>
+                                <button type="submit" class="btn ml-auto button text-center py-2 px-5 border-0">CONTINUE</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </header>
+            <header class="bank tab_contents" id="bank" style="display: none;">
+                <div class="row">
+                    <div class="col-6">
+                        <img src="https://images.africanfinancials.com/ng-guaran-logo.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-6">
+
+                        <label class="pt-2"><span class="font-weight-bold">BANK NAME:</span><br/> GUARANTEE TRUST</label>
+                        <label><span class="font-weight-bold">ACCOUNT NUMBER:</span><br/> 0423982249</label>
+                        <label><span class="font-weight-bold">ACCOUNT NAME:</span><br/> WORKPRIDE LIMITED</label>
+                        <label style="font-size: 7px; color: #f26f24;" class="font-weight-bolder">POWERED BY SELLFAST.NG</label>
+                    </div>
+                    <div class="col-md-12">
+                        <h5>2. PLEASE ENTER YOUR INFORMATION</h5>
+                        <form method="">
+                            @csrf
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for=""><i class="fa fa-user-o"></i> ACCOUNT FULL NAME</label>
+                                    <input type="text" class="form-control bg-gray" placeholder="" value="" required>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label for=""><i class="fa fa-bank"></i> BANK NAME</label>
+                                    <input type="text" class="form-control bg-gray" required>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label for=""><i class="fa fa-phone"></i> PHONE NUMBER</label>
+                                    <input type="text" class="form-control bg-gray" required>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label for=""><i class="fa fa-instagram"></i> INSTAGRAM NAME / HANDLE</label>
+                                    <input type="text" class="form-control bg-gray" required>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <label for=""><i class="fa fa-camera"></i> EVIDENCE OF PAYMENT</label>
+                                    <input type="file" class="form-control bg-gray" required>
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-end align-items-end">
+                                <div class="">
+                                    <div class="badge badge-white text-uppercase">Total Amount</div><br/>
+                                    <label class="" style="font-size: 30px">$ 500.00</label>
+                                </div>
+                                <div class="ml-auto button text-center py-2 px-5 mb-4">CONTINUE <i class="fa fa-long-arrow-right"></i></div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </header>
+
         </div>
-
-
-
-
     </div>
-</div>
+
 @endsection
 
 @section('scripts')
+<script src="https://checkout.flutterwave.com/v3.js"></script>
+<script>
+    let logo = <?php json_encode(asset('img/logo.png')) ?>
+   $(document).ready(function(){
+    let tabs = $('a');
+    let result = document.getElementsByTagName('header');
+    $(tabs).on('click', function(e) {
+        let type = e.currentTarget.dataset.type;
+        var target = $(this.rel);
+        $('.tab_contents').not(target).hide();
+        $(this.rel +'.tab_contents').show();
+        $('a.option.active').removeClass('active');
+        $(this).addClass('active');
+        $(this.rel).addClass('active');
+        $('.type').html(type);
 
+        $(this.rel+ ' form').on('submit', function(e){
+            e.preventDefault();
+            let type = e.currentTarget.dataset.type;
+            var values = {};
+            $.each($('form').serializeArray(), function(i, field) {
+                values[field.name] = field.value;
+            });
+            if(type == "card"){
+                makePayment(values);
+                //console.log(values)
+            }
+
+        })
+    });
+   })
+
+   function makePayment(e) {
+    FlutterwaveCheckout({
+      public_key: "FLWPUBK_TEST-SANDBOXDEMOKEY-X",
+      tx_ref: "hooli-tx-1920bbtyt",
+      amount: 500,
+      currency: "NGN",
+      payment_options: "card",
+      redirect_url: // specified redirect URL
+        "https://callbacks.piedpiper.com/flutterwave.aspx?ismobile=34",
+      meta: {
+        consumer_id: 23,
+        consumer_mac: "92a3-912ba-1192a",
+      },
+      customer: {
+        email: e.email_address,
+        phone_number: e.phone,
+        name: e.fullname,
+      },
+      callback: function (data) {
+        console.log(data);
+      },
+      onclose: function() {
+        // close modal
+      },
+      customizations: {
+        title: "Sellfast.ng Subscription",
+        description: "Payment for items in cart",
+        logo: logo,
+      },
+    });
+  }
+
+
+
+
+
+</script>
 @endsection
