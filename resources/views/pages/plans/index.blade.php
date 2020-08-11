934355749c68ad30c246d3543707c25adcd9d28c
dudox <trybemark@gmail.com>
2020-08-11 20:31:41 +0100

    plans overview

---
+++ resources/views/pages/plans/index.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.app')
+
+
+@section('contents')
+<div class="row justify-content-center vh-100">
+    <div class="col-md-5">
+        <div class="row  my-3 ml-1">
+            <img src="{{asset('img/logo.png')}}" style="width: 70px" alt="">
+        </div>
+
+        <div class="row mt-3">
+            <div class="col-md-12">
+                <p class="font-weight-bolder mb-0" style="font-size: 19px">Choose a plan</p>
+                <p class="text-muted">Flexible plans allows you to easily run your business ads with sellfast.ng</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-0 bg-back" data-link="{{ route('basic') }}" >
+                    <div class="card-body card1" style=" border-radius: 20px !important">
+                        <div class="row text-white">
+                            <div class="col-md-12">
+                                <h6 class="mb-0">1. BASIC PLAN</h6>
+                                <p class="mt-0 font-weight-bolder ">ONE TIME PAYMENT ADVERTISEMENT PLAN</p>
+                            </div>
+                        </div>
+                        <div class="d-flex mt-4 text-white align-items-end">
+                            <div class="amount">
+                                <h6 class="mb-0">TOTAL AMOUNT</h6>
+                                <p class="mt-0 font-weight-bolder mb-0" style="font-size: 20px">NGN 500.00</p>
+                            </div>
+                            <div class="ml-auto justify-content-end">
+                                <a class="btn btn-dark rounded-pill transparent" style="font-size: 10px">Terms & Condition <i class="fa fa-long-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row my-3">
+            <div class="col-md-12">
+                <div class="card border-0 bg-back" >
+                    <div class="card-body card2" style=" border-radius: 20px !important">
+                        <div class="row text-white">
+                            <div class="col-md-12">
+                                <h6 class="mb-0">2. PREMIUM PLAN</h6>
+                                <p class="mt-0 font-weight-bolder ">ONE TIME PAYMENT ADVERTISEMENT PLAN</p>
+                            </div>
+                        </div>
+                        <div class="d-flex mt-4 text-white align-items-end">
+                            <div class="amount">
+                                <h6 class="mb-0">TOTAL AMOUNT</h6>
+                                <p class="mt-0 font-weight-bolder mb-0" style="font-size: 20px">NGN 500.00</p>
+                            </div>
+                            <div class="ml-auto justify-content-end">
+                                <a class="btn btn-dark rounded-pill transparent" style="font-size: 10px">Terms & Condition <i class="fa fa-long-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row  " >
+            <div class="col-md-12 text-center">
+                <p class="font-weight-bolder">Powered by WorkPride</p>
+            </div>
+        </div>
+
+
+
+
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+<script>
+    $('.bg-back').on('click', function(e){
+        if(e.currentTarget.dataset.link != undefined){
+            window.location.href = e.currentTarget.dataset.link;
+        }
+    });
+</script>
+@endsection
