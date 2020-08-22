1a615f4715355c831644f0ea3ded1815fc1eef99
dudox <trybemark@gmail.com>
2020-08-22 23:21:10 +0100

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,7 +67,7 @@
         <div class="page-wrapper" >
             @yield('content')
             <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between">
-                <p class="text-muted text-center text-md-left">Copyright © 2020 <a href="https://www.workpride.net" target="_blank">Workpride</a>. All rights reserved</p>
+                <p class="text-muted text-center text-md-left">Copyright © 2020 <a href="https://www.workpride.net" target="_blank">WorkPride Limited</a>. All rights reserved</p>
                 <p class="text-muted text-center text-md-left mb-0 d-none d-md-block">SellfastNG <i class="mb-1 text-primary ml-1 icon-small" data-feather="heart"></i></p>
             </footer>
         </div>
